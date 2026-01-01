7aa47c7ef5ce674a8dc166cbf4dc06f179b51f9c
ZeeQ <kw133875@gmail.com>
2025-12-31 21:38:37 -0600

    initialized react to the project

---
+++ Typing-Test/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <div>
+        Personal best: WPM WPM: Accuracy: Time: Difficulty: Easy Medium Hard
+        Mode: Timed (60s) Passage Start Typing Test Or click the text and start
+        typing Test Complete! Solid run. Keep pushing to beat your high score.
+        WPM: Accuracy: Characters: Go Again
+        <div class="attribution">
+          Challenge by
+          <a href="https://www.frontendmentor.io?ref=challenge">
+            Frontend Mentor
+          </a>
+          . Coded by <a href="#">Your Name Here</a>.
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
